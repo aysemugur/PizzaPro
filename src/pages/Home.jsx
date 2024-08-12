5066761d7dbc36927a393ee7c95e32cc94e8ad46
Ayşem UĞUR <aysemugr@gmail.com>
2024-08-12 16:36:00 +0300

    fix sub foldering (hopefully)

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div>Home
+    {/* <div className='mainContent'></div>
+    <h2>Teknolojik Yemekler</h2>
+    <h1>KOD ACIKTIRIR, PIZZA DOYURUR</h1>
+    <button>ACIKTIM</button> */}
+    </div>
+  )
+}
+
+export default Home
